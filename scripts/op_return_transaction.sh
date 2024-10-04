31d6c49b8932901b8f40b12c54b7003c1978e58f
Rishabh Jain <rishabh.j.017@gmail.com>
2024-10-04 19:32:42 +0530

    LE-006 Fixing Bash script for OP_RETURN
         1. Added log file and improved logging for each step.
         2. Fixed corner cases
         3. Added computation logic for handling multiple UTXOs
    
    Type: FEATURE

--- scripts/op_return_transaction.sh
+++ scripts/op_return_transaction.sh
@@ -3,8 +3,11 @@
 ### Script to create an OP RETURN transaction
 ###   Arg1 : Data <= 80 bytes
 
-### Validate data length
+cli_path=$BTC_CLI_PATH
 op_return_data=$1
+num_inputs=1
+
+### Validate data length
 if [[ ${#op_return_data} -gt 80 ]]
 then
 	exit
@@ -13,21 +16,18 @@ fi
 exit_if_fail() {
 	if [[ $? -ne 0 ]]
 	then
-		>&2 echo "error" $?
+		>&2 echo "error" $1 $?
 		exit 1
 	fi
 }
 
-cli_path=$BTC_CLI_PATH
 unspent="`$cli_path listunspent > /tmp/unspent.out`"
-exit_if_fail
-# echo $unspent
-## Remove newline
-unspent=`echo ${unspent/\n/}`
-# echo $unspent
+exit_if_fail "unspent"
+
+$cli_path listunspent >> op_return_script.out
 
 change_addr=`$cli_path getrawchangeaddress`
-exit_if_fail
+exit_if_fail "getrawchangeaddress"
 
 ### Arg 1 : JSON
 ### Arg 2 : Field
@@ -36,37 +36,68 @@ find_field_from_json_dict() {
 }
 
 ### Arg 1 : JSON
-### Arg 2 : Field
+### Arg 2 : Array index
+### Arg 3 : Field
 find_field_from_json_array() {
-	jq -r ".[0] | .$2" < "/tmp/unspent.out"
+	jq -r ".[$1] | .$2" < "/tmp/unspent.out"
 }
 
 ## Get info from local wallet
+inputs=""
+total_amt=0
 
-txid=`find_field_from_json_array "$unspent" "txid"`
-vout=`find_field_from_json_array "$unspent" "vout"`
-amt=`find_field_from_json_array "$unspent" "amount"`
+calculate_required() {
+	echo $(bc <<<"(53 + $1 * 68 + $2)*0.00000001") | awk '{printf "%.8f", $0}'
+}
 
-exit_if_fail
+while :
+do
+	echo "Trying num_inputs=$num_inputs" >> op_return_script.out
+	index=$(( $num_inputs - 1 ))
+	txid=`find_field_from_json_array $index "txid"`
+	vout=`find_field_from_json_array $index "vout"`
+	amt=`find_field_from_json_array  $index "amount"`
+	exit_if_fail "getting values"
+	echo $i $txid $vout $amt >> op_return_script.out
+	total_amt=`echo $(bc <<<"$amt + $total_amt") | awk '{printf "%.8f", $0}'`
+	if [[ -z $inputs ]]
+	then
+		inputs='''{"txid":"'$txid'","vout":'$vout'}'''
+	else
+		inputs=''''$inputs',{"txid":"'$txid'","vout":'$vout'}'''
+	fi
 
-# echo $txid $vout $amt
+	required=`calculate_required $num_inputs ${#op_return_data}`
+	echo "Req: $required, Actual: $total_amt" >> op_return_script.out
+	if (( $(echo "$total_amt >= $required" | bc -l) )); then
+		break
+	fi
+	num_inputs=$(( $num_inputs + 1))
+done
 
+echo "Done: $total_amt" >> op_return_script.out
+
+inputs="[$inputs]"
+echo $total_amt $inputs >> op_return_script.out
 
 ## Create transaction
-cost=0.0001
-change=$(bc <<<"scale=10;$amt - $cost")
+cost=$required
+change=`echo $(bc <<<"$total_amt - $cost") | awk '{printf "%.8f", $0}'`
+echo "Cost: $cost, Return Change: $change" >> op_return_script.out
 
-raw_tx_hex=$($cli_path -named createrawtransaction inputs='''[ { "txid": "'$txid'", "vout": '$vout' } ]''' outputs='''{ "data": "'$op_return_data'", "'$change_addr'": "'$change'" }''')
+echo "Inputs: $inputs" >> op_return_script.out
+raw_tx_hex=$($cli_path -named createrawtransaction inputs=''''$inputs'''' outputs='''{ "data": "'$op_return_data'", "'$change_addr'": "'$change'" }''')
 
+echo "Raw tx hex: $raw_tx_hex" >> op_return_script.out
 
 ## Sign Transaction
 signed_raw_tx_hex=$($cli_path signrawtransactionwithwallet $raw_tx_hex)
-exit_if_fail
+exit_if_fail "signing transaction"
 signed_raw_tx_hex=`find_field_from_json_dict "$signed_raw_tx_hex" "hex"`
-# echo "Hex: " $signed_raw_tx_hex
-# echo "Decoded: " "`$cli_path decoderawtransaction $signed_raw_tx_hex`"
-
+echo "Signed tx: Hex: " $signed_raw_tx_hex >> op_return_script.out
+echo "Decoded tx: `$cli_path decoderawtransaction $signed_raw_tx_hex`" >> op_return_script.out
 
 ## Send Transaction
 send_raw_tx=$($cli_path sendrawtransaction $signed_raw_tx_hex)
+echo "Sent tx hex: $send_raw_tx" >> op_return_script.out
 echo $send_raw_tx
